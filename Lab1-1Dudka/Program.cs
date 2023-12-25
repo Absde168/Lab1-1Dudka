efcb72e7cbd949badc17e01018f6860c4a7d02dc
B3dRandoms <bdrandoms0@gmail.com>
2023-12-25 22:02:36 +0200

    f

---
+++ Lab1-1Dudka/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите x: ");
+double x = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите t: ");
+double t = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите c: ");
+double c = Convert.ToDouble(Console.ReadLine());
+double L = Math.Pow(1 / Math.Tan(c), 2) + ((2 * Math.Pow(x, 2) + 5) / Math.Sqrt(c + t));
+Console.WriteLine($"L = {L:F2}");
